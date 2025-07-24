c3bedca167552fd4f898d674ca444f55dfbfe397
Jon Boucher <93044611+jonboucher@users.noreply.github.com>
2025-07-24 16:48:23 -0400

    Create mock data for booklist

---
+++ backend/data/mockBookList.tsx
@@ -0,0 +1,92 @@
+export const bookData = [
+    {
+        id: 1,
+        title: "Gardens of the Moon",
+        author: "Steven Erikson",
+        cover: "https://m.media-amazon.com/images/I/71XHJct7u3L.jpg",
+        description:
+            "Vast legions of gods, mages, humans, dragons and all manner of creatures play out the fate of the Malazan Empire in this first book in a major epic fantasy series from Steven Erikson.\n\nThe Malazan Empire simmers with discontent, bled dry by interminable warfare, bitter infighting and bloody confrontations with the formidable Anomander Rake and his Tiste Andii, ancient and implacable sorcerers. Even the imperial legions, long inured to the bloodshed, yearn for some respite. Yet Empress Laseen's rule remains absolute, enforced by her dread Claw assassins.\n\nFor Sergeant Whiskeyjack and his squad of Bridgeburners, and for Tattersail, surviving cadre mage of the Second Legion, the aftermath of the siege of Pale should have been a time to mourn the many dead. But Darujhist...",
+        genre: "fantasy, grimdark",
+    },
+    {
+        id: 2,
+        title: "Deadhouse Gates",
+        author: "Steven Erikson",
+        cover: "https://m.media-amazon.com/images/I/81xAF-yaxaL.jpg",
+        description:
+            "In the vast dominion of Seven Cities, in the Holy Desert Raraku, the seer Sha'ik and her followers prepare for the long-prophesied uprising known as the Whirlwind. Unprecedented in size and savagery, this maelstrom of fanaticism and bloodlust will embroil the Malazan Empire in one of the bloodiest conflicts it has ever known, shaping destinies and giving birth to legends . . .\n\nSet in a brilliantly realized world ravaged by dark, uncontrollable magic, this thrilling novel of war, intrigue and betrayal confirms Steven Eirkson as a storyteller of breathtaking skill, imagination and originality--a new master of epic fantasy.",
+        genre: "fantasy, grimdark",
+    },
+    {
+        id: 3,
+        title: "Memories of Ice",
+        author: "Steven Erikson",
+        cover: "https://m.media-amazon.com/images/I/91vlhlzzktL.jpg",
+        description:
+            "The ravaged continent of Genabackis has given birth to a terrifying new empire: the Pannion Domin. Like a tide of corrupted blood, it seethes across the land, devouring all. In its path stands an uneasy alliance: Onearm's army and Whiskeyjack's Bridgeburners alongside their enemies of old--the forces of the Warlord Caladan Brood, Anomander Rake and his Tiste Andii mages, and the Rhivi people of the plains.\n\nBut ancient undead clans are also gathering; the T'lan Imass have risen. For it would seem something altogether darker and more malign threatens this world. Rumors abound that the Crippled God is now unchained and intent on a terrible revenge.\n\nMarking the return of many characters from Gardens of the Moon and introducing a host of remarkable new players, Memories of Ice is both a momentous new chapter in Steven Erikson's magnificent epic fantasy and a triumph of storytelling.",
+        genre: "fantasy, grimdark",
+    },
+    {
+        id: 4,
+        title: "House of Chains",
+        author: "Steven Erikson",
+        cover: "https://m.media-amazon.com/images/I/91IL6nA1IUL.jpg",
+        description:
+            "In Northern Genabackis, a raiding party of savage tribal warriors descends from the mountains into the southern flatlands. Their intention is to wreak havoc amongst the despised lowlanders, but for the one named Karsa Orlong it marks the beginning of what will prove to be an extraordinary destiny.\n\nSome years later, it is the aftermath of the Chain of Dogs. Tavore, the Adjunct to the Empress, has arrived in the last remaining Malazan stronghold of Seven Cities. New to command, she must hone twelve thousand soldiers, mostly raw recruits but for a handful of veterans of Coltaine's legendary march, into a force capable of challenging the massed hordes of Sha'ik's Whirlwind who lie in wait in the heart of the Holy Desert.\n\nBut waiting is never easy. The seer's warlords are locked into...",
+        genre: "fantasy, grimdark",
+    },
+    {
+        id: 5,
+        title: "Midnight Tides",
+        author: "Steven Erikson",
+        cover: "https://m.media-amazon.com/images/I/71fJkWcCf3L.jpg",
+        description:
+            "Vast legions of gods, mages, humans, dragons and all manner of creatures play out the fate of the Malazan Empire in this first book in a major epic fantasy series from Steven Erikson.\n\nThe Malazan Empire simmers with discontent, bled dry by interminable warfare, bitter infighting and bloody confrontations with the formidable Anomander Rake and his Tiste Andii, ancient and implacable sorcerers. Even the imperial legions, long inured to the bloodshed, yearn for some respite. Yet Empress Laseen's rule remains absolute, enforced by her dread Claw assassins.\n\nFor Sergeant Whiskeyjack and his squad of Bridgeburners, and for Tattersail, surviving cadre mage of the Second Legion, the aftermath of the siege of Pale should have been a time to mourn the many dead. But Darujhist...",
+        genre: "fantasy, grimdark",
+    },
+    {
+        id: 6,
+        title: "The Bonehunters",
+        author: "Steven Erikson",
+        cover: "https://m.media-amazon.com/images/I/71MmGV2g3DL.jpg",
+        description:
+            "Vast legions of gods, mages, humans, dragons and all manner of creatures play out the fate of the Malazan Empire in this first book in a major epic fantasy series from Steven Erikson.\n\nThe Malazan Empire simmers with discontent, bled dry by interminable warfare, bitter infighting and bloody confrontations with the formidable Anomander Rake and his Tiste Andii, ancient and implacable sorcerers. Even the imperial legions, long inured to the bloodshed, yearn for some respite. Yet Empress Laseen's rule remains absolute, enforced by her dread Claw assassins.\n\nFor Sergeant Whiskeyjack and his squad of Bridgeburners, and for Tattersail, surviving cadre mage of the Second Legion, the aftermath of the siege of Pale should have been a time to mourn the many dead. But Darujhist...",
+        genre: "fantasy, grimdark",
+    },
+    {
+        id: 7,
+        title: "Reaper's Gale",
+        author: "Steven Erikson",
+        cover: "https://m.media-amazon.com/images/I/815hegaKtPL.jpg",
+        description:
+            "Vast legions of gods, mages, humans, dragons and all manner of creatures play out the fate of the Malazan Empire in this first book in a major epic fantasy series from Steven Erikson.\n\nThe Malazan Empire simmers with discontent, bled dry by interminable warfare, bitter infighting and bloody confrontations with the formidable Anomander Rake and his Tiste Andii, ancient and implacable sorcerers. Even the imperial legions, long inured to the bloodshed, yearn for some respite. Yet Empress Laseen's rule remains absolute, enforced by her dread Claw assassins.\n\nFor Sergeant Whiskeyjack and his squad of Bridgeburners, and for Tattersail, surviving cadre mage of the Second Legion, the aftermath of the siege of Pale should have been a time to mourn the many dead. But Darujhist...",
+        genre: "fantasy, grimdark",
+    },
+    {
+        id: 8,
+        title: "Toll the Hounds",
+        author: "Steven Erikson",
+        cover: "https://m.media-amazon.com/images/I/81tf6-Ig5qL.jpg",
+        description:
+            "Vast legions of gods, mages, humans, dragons and all manner of creatures play out the fate of the Malazan Empire in this first book in a major epic fantasy series from Steven Erikson.\n\nThe Malazan Empire simmers with discontent, bled dry by interminable warfare, bitter infighting and bloody confrontations with the formidable Anomander Rake and his Tiste Andii, ancient and implacable sorcerers. Even the imperial legions, long inured to the bloodshed, yearn for some respite. Yet Empress Laseen's rule remains absolute, enforced by her dread Claw assassins.\n\nFor Sergeant Whiskeyjack and his squad of Bridgeburners, and for Tattersail, surviving cadre mage of the Second Legion, the aftermath of the siege of Pale should have been a time to mourn the many dead. But Darujhist...",
+        genre: "fantasy, grimdark",
+    },
+    {
+        id: 9,
+        title: "Dust of Dreams",
+        author: "Steven Erikson",
+        cover: "https://m.media-amazon.com/images/I/71c+Z8uBv0L._UF1000,1000_QL80_.jpg",
+        description:
+            "Vast legions of gods, mages, humans, dragons and all manner of creatures play out the fate of the Malazan Empire in this first book in a major epic fantasy series from Steven Erikson.\n\nThe Malazan Empire simmers with discontent, bled dry by interminable warfare, bitter infighting and bloody confrontations with the formidable Anomander Rake and his Tiste Andii, ancient and implacable sorcerers. Even the imperial legions, long inured to the bloodshed, yearn for some respite. Yet Empress Laseen's rule remains absolute, enforced by her dread Claw assassins.\n\nFor Sergeant Whiskeyjack and his squad of Bridgeburners, and for Tattersail, surviving cadre mage of the Second Legion, the aftermath of the siege of Pale should have been a time to mourn the many dead. But Darujhist...",
+        genre: "fantasy, grimdark",
+    },
+    {
+        id: 10,
+        title: "The Crippled God",
+        author: "Steven Erikson",
+        cover: "https://m.media-amazon.com/images/I/81Z3Ns-EliL.jpg",
+        description:
+            "Vast legions of gods, mages, humans, dragons and all manner of creatures play out the fate of the Malazan Empire in this first book in a major epic fantasy series from Steven Erikson.\n\nThe Malazan Empire simmers with discontent, bled dry by interminable warfare, bitter infighting and bloody confrontations with the formidable Anomander Rake and his Tiste Andii, ancient and implacable sorcerers. Even the imperial legions, long inured to the bloodshed, yearn for some respite. Yet Empress Laseen's rule remains absolute, enforced by her dread Claw assassins.\n\nFor Sergeant Whiskeyjack and his squad of Bridgeburners, and for Tattersail, surviving cadre mage of the Second Legion, the aftermath of the siege of Pale should have been a time to mourn the many dead. But Darujhist...",
+        genre: "fantasy, grimdark",
+    },
+];
--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,3 +1,5 @@
+import { bookData } from "../../../backend/data/mockBookList";
+
 const HomePage = () => {
     return <h1>Home Page</h1>;
 };
