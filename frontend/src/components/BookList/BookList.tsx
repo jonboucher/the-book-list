d1bc9660a4a7c2a42766861a49f7a135b67f618f
Jon Boucher <93044611+jonboucher@users.noreply.github.com>
2025-07-29 18:02:36 -0400

    Minor style updates

--- frontend/src/components/BookList/BookList.tsx
+++ frontend/src/components/BookList/BookList.tsx
@@ -16,22 +16,32 @@ type BookListProps = {
 
 const BookList = ({ books }: BookListProps) => {
     return (
-        <ul className={`${styles["book-list"]}`}>
-            {books &&
-                books.map((book) => {
-                    return (
-                        <BookListItem
-                            id={book.id}
-                            key={book.id}
-                            title={book.title}
-                            author={book.author}
-                            description={book.description}
-                            cover={book.cover}
-                            genre={book.genre}
-                        />
-                    );
-                })}
-        </ul>
+        <>
+            <h1 style={{ marginTop: "12rem" }}>Malazan: Book of the Fallen</h1>
+            <p style={{ color: "white" }}>
+                Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+                accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
+                quae ab illo inventore veritatis et quasi architecto beatae
+                vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia
+                voluptas sit aspernatur aut odit aut fugit,
+            </p>
+            <ul className={`${styles["book-list"]}`}>
+                {books &&
+                    books.map((book) => {
+                        return (
+                            <BookListItem
+                                id={book.id}
+                                key={book.id}
+                                title={book.title}
+                                author={book.author}
+                                description={book.description}
+                                cover={book.cover}
+                                genre={book.genre}
+                            />
+                        );
+                    })}
+            </ul>
+        </>
     );
 };
 
