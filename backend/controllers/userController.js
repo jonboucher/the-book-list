303fdcf19eff9f148af94c752cd614827bfac11c
Jon Boucher <93044611+jonboucher@users.noreply.github.com>
2025-08-12 16:57:43 -0400

    Add deleteUser

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -21,7 +21,6 @@ export const createUser = async (req, res) => {
         const existingUser = await sql`
             SELECT * FROM users WHERE email = ${email} OR username = ${username}
         `;
-        console.log(existingUser);
 
         if (existingUser.length > 0) {
             return res.status(409).json({
@@ -31,7 +30,6 @@ export const createUser = async (req, res) => {
         }
 
         const hashed_password = await argon2.hash(password);
-        console.log(hashed_password);
 
         const newUser = await sql`
             INSERT INTO users (username, email, password_hash)
@@ -40,6 +38,30 @@ export const createUser = async (req, res) => {
         `;
         res.status(201).json({ success: true, data: newUser[0] });
     } catch (err) {
+        res.status(500).json({
+            success: false,
+            message: "Internal server error",
+        });
+    }
+};
+
+export const deleteUser = async (req, res) => {
+    const { id } = req.body;
+
+    try {
+        const deletedUser = await sql`
+            DELETE FROM users WHERE id=${id}
+            RETURNING *
+        `;
+
+        if (deletedUser.length === 0) {
+            return res
+                .status(404)
+                .json({ success: false, message: "User not found" });
+        }
+        res.status(200).json({ success: true, data: deletedUser[0] });
+    } catch (err) {
+        console.log("Error in deleteUser function", err);
         res.status(500).json({
             success: false,
             message: "Internal server error",
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { createUser } from "../controllers/userController.js";
+import { createUser, deleteUser } from "../controllers/userController.js";
 
 const router = express.Router();
 
 router.post("/", createUser);
+router.delete("/:id", deleteUser);
 
 export default router;
